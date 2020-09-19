0ffe93b192bce7cc0e39af091ab232df2c86d927
HMoreira2020 <moreiraheather@gmail.com>
2020-09-18 18:28:11 -0700

    Done.

--- index.js
+++ index.js
@@ -1,6 +1,21 @@
-function fetchBooks() {
 
-}
+let result; 
+
+function fetchBooks() {
+  return fetch('https://anapioficeandfire.com/api/books')
+  .then(function(response) {
+  return response.json();
+  }).then(function(json) {
+    console.log(json);
+    result = json
+    // json.map(j => j["numberOfPages"]); // why can't you do this? Can you store json response in a variable? 
+    // console.log(json[4].name);
+    // console.log(json[4].numberOfPages);
+   
+    renderBooks(json);
+  })
+};
+
 
 function renderBooks(books) {
   const main = document.querySelector('main')
@@ -12,5 +27,8 @@ function renderBooks(books) {
 }
 
 document.addEventListener('DOMContentLoaded', function() {
-  fetchBooks()
+  fetchBooks();
+  console.log(result)
 })
+
+
